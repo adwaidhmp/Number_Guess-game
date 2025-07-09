2eb83d878d992c0da26a90b44a0acbdc4975c472
Adwaidh Mp <adwaidh7727@gmail.com>
2025-07-09 14:05:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Number_Guess",
+  base:"/Number_Guess-game/",
 })
